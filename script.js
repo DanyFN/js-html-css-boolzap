de91201995ba7b720df7d758735309373982cb14
Daniele <danielefontana90@gmail.com>
2020-06-16 19:18:22 +0200

    Evento Invio

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ $(document).ready(function () {
     $(".messaggio-verde-utente").show();
 
   });
+
+  $("#messaggio").keypress(function(event){
+    console.log(event.which);
+    if (event.which===13) {
+      console.log("ciao");
+      var testoMessaggio = $("#messaggio").val();
+      var cloneMessaggio = $(".messaggio-verde-utente .to-do-item").text(testoMessaggio);
+      console.log(cloneMessaggio);
+      $("to-do p").append(cloneMessaggio);
+      $(".messaggio-verde-utente").show();
+
+    }
+
+  });
+
+
+
+
 });
