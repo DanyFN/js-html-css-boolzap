24aa8a2c7db68e2624dd987e3dcaa9bf721a912c
Daniele <danielefontana90@gmail.com>
2020-06-16 18:33:39 +0200

    Evento Click

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+$(document).ready(function () {
+  $(".messaggio-verde-utente").hide();
+  $("#click-messaggio").click(function(){
+    var testoMessaggio = $("#messaggio").val();
+    var cloneMessaggio = $(".messaggio-verde-utente .to-do-item").text(testoMessaggio);
+    $("to-do p").append(cloneMessaggio);
+    $(".messaggio-verde-utente").show();
 
+  });
+});
