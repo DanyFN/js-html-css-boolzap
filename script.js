1383851c03d6bf2d8ef165162fefa9f1af951c65
Daniele <danielefontana90@gmail.com>
2020-06-17 17:32:00 +0200

    Total Fix Boolzap #1

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 $(document).ready(function () {
-  $(".messaggio-verde-utente").hide();
-  $("#click-messaggio").click(function(){
-    var testoMessaggio = $("#messaggio").val();
-    var cloneMessaggio = $(".messaggio-verde-utente .to-do-item").text(testoMessaggio);
-    $("to-do p").append(cloneMessaggio);
-    $(".messaggio-verde-utente").show();
 
+  $(document).on("click","#click-messaggio",function(){
+    invioMessaggioUtente();
   });
 
-  $("#messaggio").keypress(function(event){
-    console.log(event.which);
-    if (event.which===13) {
-      console.log("ciao");
-      var testoMessaggio = $("#messaggio").val();
-      var cloneMessaggio = $(".messaggio-verde-utente .to-do-item").text(testoMessaggio);
-      console.log(cloneMessaggio);
-      $("to-do p").append(cloneMessaggio);
-      $(".messaggio-verde-utente").show();
-
+  $("#input-messaggio").keypress(function(event){
+    if (event.which===13 || event.keycode===13) {
+      invioMessaggioUtente();
     }
-
   });
 
+  function invioMessaggioUtente(){
+    var chatMessaggio = $("#input-messaggio").val();
+    // se la chat non è vuota allora clonami il messaggio
+    if (chatMessaggio != "") {
+      var nuovoMessaggio = $(".template .messaggio").clone();
+      // prendo il figlio di messaggio(testo-messaggio) e inserisco
+      // il valor dell input messaggio
+      nuovoMessaggio.children(".testo-messaggio").text(chatMessaggio);
+      //aggiungo la classe inviato
+      nuovoMessaggio.addClass("inviato");
+      var data = new Date();
+      var oraCorrente = data.getHours();
+      var minutoCorrente = data.getMinutes();
+      var oraEsatta = aggiungiZero(oraCorrente) + ":" + aggiungiZero(minutoCorrente);
+      nuovoMessaggio.children(".ora-messaggio").text(oraEsatta);
+      $(".finestra-chat").append(nuovoMessaggio);
+      //reset dell input messaggio
+      $("#input-messaggio").val("");
+      $(".finestra-chat").scrollTop($(".finestra-chat").height());
+    }
+  }
 
-
+  function aggiungiZero(num){
+    if (num < 10) {
+      return "0" + num;
+    }
+    return num;
+  }
 
 });
